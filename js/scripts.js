fb1d9525a52f17c427e55ecf224e6684e1cc2f6c
Dave Everitt <deveritt@innotts.co.uk>
2018-10-28 15:19:31 +0000

    first exercise code done

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,10 @@
-const hello = document.getElementById("hello");
+const menu = document.getElementById("menu");
 
-//uncomment below to see what getElementById 'got':
 //console.log(hello);
 
 function greeting() {
   alert("Hello!");
-  hello.textContent = "Goodbye";
+  hello.innerText = "Goodbye";
 }
 
 hello.addEventListener("click", greeting);
-
-/*
- for textContent vs innerHTML vs innerText see:
- https://developer.mozilla.org/en-US/docs/Web/API/Node/textContent#Differences_from_innerText
-*/
