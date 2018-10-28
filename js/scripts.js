2e0d67a81ef1b13d86f4151ccc22c3eeda2c43be
Dave Everitt <deveritt@innotts.co.uk>
2018-10-28 17:05:16 +0000

    second exercise code done

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,9 @@
-const menu = document.getElementById("menu");
+const menuToggle = document.getElementById("menu-toggle");
+const menuNav = document.getElementById("menu-nav");
 
+const toggleMenu = () => {
+  console.log("called toggleMenu")
+  menuNav.classList.toggle("menu-toggle");
+}
+
+menuToggle.addEventListener("click", toggleMenu);
--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
 # Code for CTEC3905 04 lab
 
+GIT branch: JavaScript show-hide menu
