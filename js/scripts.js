02d02ad34714e9d8d6d5dd66b896ece494988e97
Dave Everitt <deveritt@innotts.co.uk>
2018-10-28 15:47:14 +0000

    remove redundant code and simplify

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,2 @@
 const menu = document.getElementById("menu");
 
-//console.log(hello);
-
-function greeting() {
-  alert("Hello!");
-  hello.innerText = "Goodbye";
-}
-
-hello.addEventListener("click", greeting);
